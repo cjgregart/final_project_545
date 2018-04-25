0e2c713920dd6dacc10eeb54b99a34cc3956c0d7
CJ Gregart <cjgregart@gmail.com>
2018-04-24 18:25:07 -0700

    just added notes

--- SF_restaurants_project.R
+++ SF_restaurants_project.R
@@ -28,3 +28,8 @@ SF + geom_point(data = data,
 #1 set colors to risk_category (dark = bad, green = good) 
 #2 set size or color or something to inspection_score; or make levels (e.g. 0-10, 10-20, ... 90-100)
 #3 look up lat/long of blank rows by address to fill in missing 20,000 values
+    # a. skip rows with "off the grid" for address, or look up by business name
+    # b. skip rows that already have lat/lon; roughly 25k remaining
+    # c. at 2,500 per person per day, should only take 2 days worth of running queries by each of us
+
+    # this accomplishes some "data cleaning"
