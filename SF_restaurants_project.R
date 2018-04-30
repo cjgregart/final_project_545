b081257ef572d3e92920153d6118f1b74b40ad06
CJ Gregart <cjgregart@gmail.com>
2018-04-30 02:05:41 -0700

    improved maps (fill = score category), added vermin map, added low score map

--- SF_restaurants_project.R
+++ SF_restaurants_project.R
@@ -84,22 +84,88 @@ SF = qmap("San Francisco",
 SF
 
 
-SF + geom_point(data = data4, 
+data6 = data4
+
+
+data6 = data6 %>%
+  filter(data6$inspection_score != "NA")
+
+data6$gr = cut(data6$inspection_score, 
+               breaks = c(39,49,59,69,79,89,99,100),
+               labels = c(40,50,60,70,80,90,100))
+
+View(data6)
+
+SF + geom_point(data = data6, 
                 aes(x= business_longitude, 
-                    y = business_latitude),
-                alpha = 0.05,
-                color = "darkred",
-                size = 2)
+                    y = business_latitude,
+                fill = data6$gr),
+                alpha = 1,
+                
+                shape = 21,
+                size = 2
+                ) +
+  scale_fill_manual(values = c("darkred",
+                               "red",
+                               "darkorange",
+                               "orange",
+                               "yellow",
+                               "green",
+                               "darkgreen"))
+
+# map of low scores
+
+View(data6)
+
+data_lowIS = data6 %>%
+  filter(data6$inspection_score < 60)
+
+  # selected threshold = 60
+
+SF + geom_point(data = data_lowIS, 
+                aes(x= business_longitude, 
+                    y = business_latitude,
+                    fill = data_lowIS$gr),
+                alpha = 0.1,
+                shape = 21,
+                size = 2
+) +
+  scale_fill_manual(values = c("darkred",
+                               "red",
+                               "darkorange",
+                               "orange",
+                               "yellow",
+                               "green",
+                               "darkgreen"))
+
+# vermin map
+
+data_vermin = data6 %>%
+  filter(data6$violation_description == "High risk vermin infestation")
 
 
-#1 set colors to risk_category (dark = bad, green = good) 
-#2 set size or color or something to inspection_score; or make levels (e.g. 0-10, 10-20, ... 90-100)
+SF + geom_point(data = data_vermin, 
+                aes(x= business_longitude, 
+                    y = business_latitude,
+                    fill = "darkred"),
+                alpha = 0.4,
+                shape = 21,
+                size = 2
+)
 
 
+
+
+
+
+
+
+# bar chart of distribution of violations in each risk category
 
 data5 = data4 %>%
   filter(data4$risk_category != "")
 
+View(data5)
 
 risk_hist = ggplot(data5, aes(x = data5$risk_category)) +
   geom_histogram(stat = "count") +
@@ -141,3 +207,20 @@ score_dist = ggplot(data_scores3,
 
 
 score_dist
+
+View(data3)
+
+
+
+#### TO DO
+
+# 1.  make a shiny app for map of violations, 
+#     where you can select/filter by: 
+#       -violation type (e.g. "High risk vermin investation")
+#       -violate risk level (low, med, high)
+#       -violation score range (0-100)
+#       -inspection date range (e.g. 2016 - 2017)
+
+# 2.  group all inspections per location (business name) 
+#     & date into single observation (combine name + date)
+#     and include score, etc. 
